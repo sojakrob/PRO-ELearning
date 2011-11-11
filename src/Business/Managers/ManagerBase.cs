4d214dc75fbfbf9c45fe626f350e5924e942e554
Robert Sojak <sojakrob@fel.cvut.cz>
2011-11-11 13:24:59 +0100

    Project sources moved to src folder

---
+++ src/Business/Managers/ManagerBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ELearning.Business.Repositories;
+using ELearning.Business.Storages;
+using ELearning.Data;
+using System.Linq.Expressions;
+
+namespace ELearning.Business.Managers
+{
+    public abstract class ManagerBase<T> : IRepository<T> where T : class
+    {
+        private IPersistentStorage _persistentStorage;
+
+        /// <summary>
+        /// Gets current data context
+        /// </summary>
+        protected DataModelContainer Context
+        {
+            get { return _persistentStorage.GetDataContext(); }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the ManagerBase class.
+        /// </summary>
+        /// <param name="persistentStorage"></param>
+        public ManagerBase(IPersistentStorage persistentStorage)
+        {
+            _persistentStorage = persistentStorage;
+        }
+
+
+
+        #region IRepository<T> Members
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return Context.CreateObjectSet<T>();
+            // TODO Apply rights based on current user type
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate)
+        {
+            return GetAll().SingleOrDefault<T>(predicate);
+        }
+
+        #endregion
+    }
+}
